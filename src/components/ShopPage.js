f26d23a2e7bd453b60a80af62b76b602ea8e5120
robbee72 <krobnson@gmail.com>
2018-11-27 09:53:42 -0600

    card flip on shop

--- src/components/ShopPage.js
+++ src/components/ShopPage.js
@@ -3,14 +3,28 @@ import { NavLink } from "react-router-dom";
 
 const ShopPage = () => (
   <div className="box-layout__shop">
-    <div className="box-layout__box">
-      <div className="content-container">
-        <NavLink className="header__title" to="/" />
-        <h1>The Fly Shop</h1>
-        <p>The Art of Fly Fishing</p>
+    <div class="content__content">
+      <NavLink className="header__title" to="/" />
+
+      <div class="card">
+        <div class="card__content">
+          <div class="card__front">
+            <h3 class="card__title">The Fly Shop</h3>
+            <p class="card__subtitle">The Art of Fly Fishing</p>
+          </div>
+
+          <div class="card__back">
+            <p class="card__body">
+              “You did not kill the fish only to keep alive and to sell for
+              food, he thought. You killed for pride and because you are a
+              fisherman. You loved him when he was alive and you loved him
+              after. If you love him, it is not a sin to kill him. Or is it
+              more?” -- Ernest Hemingway
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 );
-
 export default ShopPage;
