2123abbd332bfbf07f7f96c40251eaa47cc95465
robbee72 <krobnson@gmail.com>
2018-12-10 09:00:33 -0600

    vscode

--- src/components/DiningPage.js
+++ src/components/DiningPage.js
@@ -6,7 +6,7 @@ const DiningPage = () => (
     <div className="box-layout__box">
       <div className="content-container">
         <NavLink className="header__title" to="/" />
-        <h1 className="box-layout__title">le Cafe des Grille</h1>
+     <h1 className="box-layout__title">le Cafe des Grille</h1>
         <p>Chef Barasso from breakfast to Last night dining.</p>
       </div>
     </div>
